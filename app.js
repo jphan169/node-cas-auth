6347c21e31860f52356477a5285d201912c6102c
Jphan169 <tung.son169@gmail.com>
2020-08-28 21:24:49 +0700

    9th change

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var session = require('express-session');
+var CASAuthentication = require('node-cas-authentication');
+var cas = new CASAuthentication({
+    cas_url     : 'https://hadtech-cas.tk/cas',
+    service_url : 'https://node-cas.herokuapp.com'
+}); 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,7 +26,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.get('/login', cas.bounce, function ( req, res ) {
+    res.send( '<html><body>Hello!</body></html>' );
+});
+ 
+// Unauthenticated clients will receive a 401 Unauthorized response instead of
+// the JSON data.
+app.get('/api', cas.block, function ( req, res ) {
+    res.json( { success: true } );
+});
+ 
+// An example of accessing the CAS user session variable. This could be used to
+// retrieve your own local user records based on authenticated CAS username.
+app.get('/api/user', cas.block, function ( req, res ) {
+    res.json( { cas_user: req.session[ cas.session_name ] } );
+});
+ 
+// Unauthenticated clients will be redirected to the CAS login and then to the
+// provided "redirectTo" query parameter once authenticated.
+app.get('/authenticate', cas.bounce_redirect );
+ 
+// This route will de-authenticate the client with the Express server and then
+// redirect the client to the CAS logout page.
+app.get('/logout', cas.logout );
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -38,9 +65,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 //var app = require('express')();
-var session = require('express-session');
-var CASAuthentication = require('node-cas-authentication');
- 
+
 // Set up an Express session, which is required for CASAuthentication.
 app.use( session({
     secret            : 'super secret key',
@@ -49,34 +74,9 @@ app.use( session({
 }));
  
 // Create a new instance of CASAuthentication.
-var cas = new CASAuthentication({
-    cas_url     : 'https://hadtech-cas.tk/cas',
-    service_url : 'https://node-cas.herokuapp.com'
-});
+
  
 // Unauthenticated clients will be redirected to the CAS login and then back to
 // this route once authenticated.
-app.get( '/login', cas.bounce, function ( req, res ) {
-    res.send( '<html><body>Hello!</body></html>' );
-});
- 
-// Unauthenticated clients will receive a 401 Unauthorized response instead of
-// the JSON data.
-app.get( '/api', cas.block, function ( req, res ) {
-    res.json( { success: true } );
-});
- 
-// An example of accessing the CAS user session variable. This could be used to
-// retrieve your own local user records based on authenticated CAS username.
-app.get( '/api/user', cas.block, function ( req, res ) {
-    res.json( { cas_user: req.session[ cas.session_name ] } );
-});
- 
-// Unauthenticated clients will be redirected to the CAS login and then to the
-// provided "redirectTo" query parameter once authenticated.
-app.get( '/authenticate', cas.bounce_redirect );
- 
-// This route will de-authenticate the client with the Express server and then
-// redirect the client to the CAS logout page.
-app.get( '/logout', cas.logout );
+
 module.exports = app;
